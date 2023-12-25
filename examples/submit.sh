f19372b9fb2ee20363f4b325a973e37dba099eef
micgre93 <michaeljgreenburg@gmail.com>
2023-12-25 14:18:02 -0700

    'Finalized' submission script

--- examples/submit.sh
+++ examples/submit.sh
@@ -4,41 +4,53 @@ dir="$(dirname "$0")"
 
 module purge
 module load julia parallel slurm-auto-array
+mkdir -p job-logs
 
-mirror_params='::: 3 10 30 ::: 100 ::: 0.01 0.1 0.5 ::: 1 3 10'
+#                  radius      nrings  roughness rms      roughness sigma
+mirror_params='::: 3 10 30 ::: 100 ::: 0 0.01 0.1 ::: 1 3 10'
+#                  angle             beam sigma
 efield_params='::: 0 15 30 45 75 ::: 0 1 3 10'
 
 
 
 submit_array_job() {
-    script="$1"
-    params="$2"
-    shift; shift
-    bash -c "parallel echo $params" | slurm-auto-array --qos standby \
-                                                       --requeue \
+    local script="$1"
+    local params="$2"
+    local t="$3"
+    local n="$4"
+    local mem="$5"
+    shift 5
+
+    local threads=auto
+    grep -qE 'current$' <<< "$script" && threads=1
+
+    bash -c "parallel echo $params" | slurm-auto-array --time "$t" \
+                                                       --ntasks "$n" --nodes 1 \
+                                                       --mem "$mem" \
+                                                       -J "${script#generate_}" \
+                                                       -l "job-logs/$script-%A.log" \
+                                                       -o "job-logs/$script-%A-%a.out" \
                                                        --parsable \
-                                                       -J "$script" \
-                                                       -o "$script-%A-%a.out" \
                                                        "$@" -- \
-                                                       julia --project="$dir/.." --threads=auto "$dir/$script"
+                                                 julia --project="$dir/.." \
+                                                       --threads="$threads" \
+                                                       --heap-size-hint="$mem" \
+                                                       "$dir/$script"
 }
 
 
 
-mirror_job_id="$(     submit_array_job generate_mirror      "$mirror_params" \
-                                       -t 1  --mem 1G)"
+mirror_job_id="$(submit_array_job generate_mirror "$mirror_params" 5 1 2G)"
 
-impedance_job_id="$(  submit_array_job generate_impedance   "$mirror_params" \
-                                       -t 60 --mem 250G -N 1 -n 100 -d afterok:$mirror_job_id)"
+impedance_job_id="$(submit_array_job generate_impedance "$mirror_params" 90 50 450G -d afterok:$mirror_job_id)"
 
-efield_job_id="$(     submit_array_job generate_efield      "$efield_params $mirror_params" \
-                                       -t 1  --mem 1G               -d afterok:$mirror_job_id)"
+efield_job_id="$(submit_array_job generate_efield "$efield_params $mirror_params" 5 1 2G -d afterok:$mirror_job_id)"
 
-current_job_id="$(    submit_array_job generate_current     "$efield_params $mirror_params" \
-                                       -t 60 --mem 500G -N 1 -n 120 -d afterok:$impedance_job_id:$efield_job_id)"
+current_job_id="$(submit_array_job generate_current "$efield_params $mirror_params" 90 128 500G \
+                  -d afterok:$impedance_job_id:$efield_job_id)"
 
-reflectance_job_id="$(submit_array_job generate_reflectance "$efield_params $mirror_params" \
-                                       -t 10 --mem 10G -d afterok:$current_job_id)"
+reflectance_job_id="$(submit_array_job generate_reflectance "$efield_params $mirror_params" 30 1 2G \
+                      -d afterok:$current_job_id)"
 
 
 
